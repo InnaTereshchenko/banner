27578f47d1b25e48c5f2c38b4034c23572d9b6be
Inna <inna.tereshchenko1@gmail.com>
2020-02-20 09:06:46 +0200

    Updates

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const close = document.getElementById('close');
 const banner = document.getElementById('banner');
+const watch = document.getElementById('watch');
+const video = document.getElementById('video');
+let isWatching = false;
+const videoDimmer = document.getElementById('dimmer');
 
 const closeBanner = () => {
   banner.style.display = 'none';
@@ -7,5 +11,20 @@ const closeBanner = () => {
 
 close.addEventListener('click', closeBanner, true);
 
-console.log(close);
-console.log(banner);
+const handleButton = () => {
+  if (isWatching) {
+    video.style.display = 'none';
+    watch.style.transform = 'rotate(45deg)';
+    isWatching = false;
+    videoDimmer.style.display = 'none';
+  } else {
+    video.style.display = 'block';
+    videoDimmer.style.display = 'block';
+    watch.style.transform = 'rotate(45deg)';
+    isWatching = true;
+  }
+};
+
+watch.addEventListener('click', handleButton);
+
+video.addEventListener('click', () => video.play());
