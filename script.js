67c2e436e6df5ec034c69f01f805c53c7892d951
Inna <inna.tereshchenko1@gmail.com>
2020-02-20 11:27:20 +0200

    Updates

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const watch = document.getElementById('watch');
 const video = document.getElementById('video');
 let isWatching = false;
 const videoDimmer = document.getElementById('dimmer');
+const playButton = document.getElementById('play-button');
+const text = document.getElementById('text');
 
 const closeBanner = () => {
   banner.style.display = 'none';
@@ -14,17 +16,24 @@ close.addEventListener('click', closeBanner, true);
 const handleButton = () => {
   if (isWatching) {
     video.style.display = 'none';
+    video.pause();
     watch.style.transform = 'rotate(45deg)';
     isWatching = false;
+    playButton.style.display = 'none';
     videoDimmer.style.display = 'none';
   } else {
     video.style.display = 'block';
     videoDimmer.style.display = 'block';
+    playButton.style.display = 'block';
     watch.style.transform = 'rotate(45deg)';
+    text.style.display = 'none';
     isWatching = true;
   }
 };
 
 watch.addEventListener('click', handleButton);
 
-video.addEventListener('click', () => video.play());
+playButton.addEventListener('click', () => {
+  playButton.style.display = 'none';
+  video.play();
+});
